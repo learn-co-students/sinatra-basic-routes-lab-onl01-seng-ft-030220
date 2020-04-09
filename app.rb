5abc1a0a108177fafec314665505e908bada7b16
marsha <marshaannon@gmail.com>
2020-04-09 15:03:02 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get'/name' do
+        "My name is Marsha"
+    end
+
+    get'/hometown' do
+        "My hometown is Hatfield, Herts, UK"
+    end
+
+    get'/favorite-song' do
+        "My favorite song is Figure it Out"
+    end
+    
 end
