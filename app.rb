456e65b4eeac7c3f77e2b4daa3fc187e21c469ca
matt_kuras <mattkuras@gmail.com>
2020-04-10 15:31:48 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+    
+    get '/name' do 
+        "My name is Matt"
+    end
+
+    get '/hometown' do 
+        "My hometown is Brick"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Ms. New Booty"
+    end
 end
